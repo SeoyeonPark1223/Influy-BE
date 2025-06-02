5c0b7878ee0af2da10f16f896f3dcd4156361479
SeoyeonPark1223 <trispark1223@gmail.com>
2025-06-02 20:02:59 +0900

    Refactor: 필요없는 라이브러리 삭제

--- src/main/java/com/influy/domain/faqCard/controller/FaqCardRestController.java
+++ src/main/java/com/influy/domain/faqCard/controller/FaqCardRestController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Check;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
