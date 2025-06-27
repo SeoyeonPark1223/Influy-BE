b2e8b9d41cf4794438ed9b1de8f73b700cde6b8c
SeoyeonPark1223 <trispark1223@gmail.com>
2025-06-27 17:47:29 +0900

    Feature: 찜 관련 파일 세팅

---
+++ src/main/java/com/influy/domain/like/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.influy.domain.like.repository;
+
+import com.influy.domain.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
