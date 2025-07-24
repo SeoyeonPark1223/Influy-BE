23fcd6ef663cd2899a7cb2cae4c91cce78256545
SeoyeonPark1223 <trispark1223@gmail.com>
2025-07-24 22:02:47 +0900

    Feature: 검색 관련 dto 세팅중

--- src/main/java/com/influy/domain/search/dto/SearchResponseDto.java
+++ src/main/java/com/influy/domain/search/dto/SearchResponseDto.java
@@ -1,5 +1,6 @@
 package com.influy.domain.search.dto;
 
+import com.influy.domain.item.dto.ItemResponseDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
@@ -10,15 +11,17 @@ public class SearchResponseDto {
         private List<SellerPageResultDto> sellerPageDtoList;
 
         @Schema(description = "아이템 리스트")
-        private List<ItemPageResultDto> itemPageDtoList;
+        private List<ItemResponseDto.HomeItemViewPageDto> itemPageDtoList;
     }
 
     public static class SellerPageResultDto {
+        @Schema(description = "셀러 리스트")
+        private List<SellerResultDto> sellerDtoList;
 
-    }
-
-    public static class ItemPageResultDto {
-        @Schema(description = "아이템 리스트")
-        private <HomeItemViewPageDto>
+        private Integer listSize;
+        private Integer totalPage;
+        private Long totalElements;
+        private Boolean isFirst;
+        private Boolean isLast;
     }
 }
