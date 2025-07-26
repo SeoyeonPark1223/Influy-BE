c2966a6ac16d3984739285569f9901556462a6c5
challonsy <musain2019@naver.com>
2025-07-26 19:07:39 +0900

    #64 fix : antMatcher에 맞게 shouldNotFilter 경로 수정

--- src/main/java/com/influy/global/util/StaticValues.java
+++ src/main/java/com/influy/global/util/StaticValues.java
@@ -7,10 +7,10 @@ public class StaticValues {
     public static final long REFRESH_EXPIRE = 1000L * 60 * 60 * 24 * 14; // 2주
     // 경로 + 허용 메서드 조합 정의
     public static final String[] SHOULD_NOT_FILTER_LIST = {
-            "/member/register",
+            "/member/register/*",
             "/member/auth/reissue",
-            "/swagger-ui",
-            "/api-docs",
+            "/swagger-ui/**",
+            "/api-docs/**",
             "/v3/api-docs",
             "/favicon.ico",
             "/seller/*/market-links",
